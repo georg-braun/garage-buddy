0ae6eb797181d025439970a229c6573e3f3902c6
Georg <Georg.Braun92@gmail.com>
2021-11-02 19:09:33 +0100

    add chakra ui and created login screen

--- WebApp/pages/index.tsx
+++ WebApp/pages/index.tsx
@@ -1,13 +1,26 @@
 import Head from 'next/head';
 
-import Layout, { siteTitle } from '@/components/layout';
+import { useAuth } from '@/lib/auth';
+
+import React from 'react';
+import { Box, Flex, Heading } from '@chakra-ui/layout';
+
+import { Button } from '@chakra-ui/button';
+import Layout from '@/components/layout';
 
 export default function Home({}) {
     return (
         <Layout>
+            <p>Hi garage buddy</p>
+        </Layout>
+
+        /*
+        <Layout>
             <Head>
                 <title>{siteTitle}</title>
             </Head>
+            <main>Hello</main>
         </Layout>
+        */
     );
 }
